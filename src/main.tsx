4c5ac0bef43fcf36fbe678dad3a29108bd30f3f3
rehanmalik-1590 <rehanmalikiubroll21@gmail.com>
2026-07-17 17:09:17 +0300

    all chages updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 /* // ......................main.tsx file ............................. */
 import { createRoot } from 'react-dom/client'
-// import "react-toastify/dist/ReactToastify.css";
-// import './index.css'
+import "react-toastify/dist/ReactToastify.css";
+import './index.css'
 import App from './App'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
