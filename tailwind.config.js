c04b51740b88a14ceb6e423c9697a2f40bd7ccf6
rehanmalik-1590 <rehanmalikiubroll21@gmail.com>
2026-07-17 18:48:14 +0300

    Update2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./docs/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/RFID-ERP-Dashboard-with-AI-Chatbot/',
   server: {
     port: 2000,
     host: true
   },
-plugins: [
-  tailwindcss(),
-  react()
-],
+  plugins: [
+    react()
+  ],
   build: {
-  outDir: 'docs',
-  sourcemap: false,
-  chunkSizeWarningLimit: 1000,
-  rollupOptions: {
-    output: {
-      manualChunks: {
-        vendor: ['react', 'react-dom', '@mui/material', '@mui/icons-material'],
-        recharts: ['recharts'],
-        mui: ['@mui/x-data-grid', '@mui/x-date-pickers']
+    outDir: 'docs',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@mui/material', '@mui/icons-material'],
+          recharts: ['recharts'],
+          mui: ['@mui/x-data-grid', '@mui/x-date-pickers']
+        }
       }
     }
   }
-}
-});
+})
