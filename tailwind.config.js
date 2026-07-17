e7730801278ae8422fe5f1edc8451cfb7871b111
rehanmalik-1590 <rehanmalikiubroll21@gmail.com>
2026-07-17 17:46:04 +0300

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js - Root folder mein (project ke root mein)
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
